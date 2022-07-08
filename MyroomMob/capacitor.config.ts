8d87bcc307946dc4d629bcc81889d2811fe0ff2a
prashanth <prashanthmp009@gmail.com>
2022-07-08 21:41:09 +0530

    myroom

---
+++ MyroomMob/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MyroomMob',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
